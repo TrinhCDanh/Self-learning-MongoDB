7bce63c92f87e3b4e95962354b4900662d2c1e21
TrinhCDanh <trinhdanh04@gmail.com>
2018-02-25 23:21:53 +0700

    find by _id

--- 04-find/find.js
+++ 04-find/find.js
@@ -8,3 +8,15 @@ personModel.findOne({name: 'Toshiba'}, (err, result) => {
     if (err) return handleError(err);
     console.log(`Result: ${result.name + result._id}`);
 });
+
+var character = new personModel({
+    name: 'MSI'
+});
+
+character.save().then(() => console.log('Success'));
+
+console.log(character._id);
+// personModel.findOne({_id: character._id.toString()}, (err, result) => {
+//     if (err) return handleError(err);
+//     console.log(`Result: ${result.name}`);
+// });
